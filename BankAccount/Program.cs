49fee38473f097eaa9ec85f6132ac62415b5765a
JEFF ECKMAN <netspy03@msn.com>
2021-03-23 20:42:21 -0700

    Completed Account class

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -10,6 +10,44 @@ namespace BankAccount
     {
         static void Main(string[] args)
         {
+            Account robsAccount = new Account();
+            if (robsAccount.WithdrawFunds(0))
+            {
+                Console.WriteLine("Cash withdrawn");
+            }
+            else
+            {
+                Console.WriteLine("Insuficient funds");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Account
+    {
+        private decimal balance = 0;
+
+        public bool WithdrawFunds(decimal amount)
+        {
+            if (balance < amount)
+            {
+                return false;
+            }
+
+            balance = balance - amount;
+
+            return true;
+        }
+
+        public void PayInFunds(decimal amount)
+        {
+            balance = balance + amount;
+        }
+
+        public decimal GetBalance()
+        {
+            return balance;
         }
     }
 }
